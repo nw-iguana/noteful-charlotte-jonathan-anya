2fca79dc5293c9e57ec5786c9183fcaeb499daf4
Charlotte Shih <shih.charlotte@gmail.com>
2019-04-02 22:18:50 -0700

    tried implementing delete button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Nav from './Components/Nav';
 import Folder from './Components/Folder';
 import Main from './Components/Main';
 import NotePage from './Components/NotePage';
-import dummyStore from './dummy-store';
+// import dummyStore from './dummy-store';
 import AppContext from './AppContext'
 
 
@@ -15,12 +15,27 @@ import AppContext from './AppContext'
 class App extends Component {
   state = {
     folders: [],
-    notes: []
+    notes: [],
   }
 
   componentDidMount() {
-    // fake date loading from API call
-    setTimeout(() => this.setState(dummyStore), 600)
+    fetch('http://localhost:9090/folders')
+    .then(response => response.json())
+    .then(responseJson => this.setState({
+      folders: responseJson
+    }));
+
+    fetch('http://localhost:9090/notes')
+    .then(response => response.json())
+    .then(responseJson => this.setState({
+      notes: responseJson
+    }));
+  }
+
+  handleDelete = (responseJson) => {
+    this.setState({
+      notes: responseJson
+    })
   }
 
   render() {
@@ -33,18 +48,15 @@ class App extends Component {
           <Nav />
           <section className="app-content">
             <main>
-                <Route
-                  exact path="/"
-                  render={routeProps => <Main {...routeProps} />}
-                />
-                <Route
-                  path="/folder/:folderId"
-                  render={routeProps => <Folder {...routeProps} />}
-                />
-                <Route
-                  path="/note/:noteId"
-                  render={routeProps => <NotePage {...routeProps} />}
-                />
+              <Route
+                exact path="/"
+                render={routeProps => <Main handleDelete={this.handleDelete} {...routeProps} />} />
+              <Route
+                path="/folder/:folderId"
+                component={Folder} />
+              <Route
+                path="/note/:noteId"
+                component={NotePage} />
             </main>
           </section>
         </div>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Main.css';
 import AppContext from '../AppContext';
 
-export default function Main() {
-    return (
-        <AppContext.Consumer>
-            {({ notes }) => notes.map((note, index) => {
-                return (
-                    <main className="notes-display" key={index}>
-                        <section className="note">
-                            <Link to={`/note/${note.id}`}>
-                                <h3>{note.name}</h3>
-                            </Link>
-                            <p>Date Modified: {note.modified}</p>
-                            <button className="delete-button">Delete Note</button>
-                        </section>
-                        <button>Add Note</button>
-                    </main>
-                )
-            })}
-        </AppContext.Consumer>
-    )
+export default class Main extends Component {
+
+    handleDeleteButton = (e, id) => {
+        const url = `http://localhost:9090/notes/${id}`
+        fetch(url, {
+            method: 'DELETE'
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(response.statusText);
+            }
+        })
+        .then(responseJson => this.props.handleDelete(responseJson));
+    }
+
+    render() {
+        return (
+            <AppContext.Consumer>
+                {({ notes }) => {
+                    return (notes.map((note, index) => {
+                        return (
+                            <main className="notes-display" key={index}>
+                                <section className="note">
+                                    <Link to={`/note/${note.id}`}>
+                                        <h3>{note.name}</h3>
+                                    </Link>
+                                    <p>Date Modified: {note.modified}</p>
+                                    <button onClick={(e, id) => this.handleDeleteButton(e, note.id)} className="delete-button">Delete Note</button>
+                                </section>
+                                <button>Add Note</button>
+                            </main>
+                        )
+                    })
+                )} }
+            </AppContext.Consumer>
+        )
+    }
 }
