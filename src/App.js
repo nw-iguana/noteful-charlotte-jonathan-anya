05808f96b3d3899e5867110785cb649708bc74ab
Alon Cohen <alon@cohencodes.com>
2019-04-03 21:57:49 -0700

    fixed delete button functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Nav from './Components/Nav';
 import Folder from './Components/Folder';
 import Main from './Components/Main';
 import NotePage from './Components/NotePage';
 // import dummyStore from './dummy-store';
-import AppContext from './AppContext'
-
+import AppContext from './AppContext';
 
 // why doesn't this work here if it's a global variable?
 // const AppContext = React.createContext();
@@ -15,48 +14,52 @@ import AppContext from './AppContext'
 class App extends Component {
   state = {
     folders: [],
-    notes: [],
-  }
+    notes: []
+  };
 
   componentDidMount() {
     fetch('http://localhost:9090/folders')
-    .then(response => response.json())
-    .then(responseJson => this.setState({
-      folders: responseJson
-    }));
+      .then(response => response.json())
+      .then(responseJson =>
+        this.setState({
+          folders: responseJson
+        })
+      );
 
     fetch('http://localhost:9090/notes')
-    .then(response => response.json())
-    .then(responseJson => this.setState({
-      notes: responseJson
-    }));
+      .then(response => response.json())
+      .then(responseJson =>
+        this.setState({
+          notes: responseJson
+        })
+      );
   }
 
-  handleDelete = (responseJson) => {
+  handleDelete = id => {
     this.setState({
-      notes: responseJson
-    })
-  }
+      notes: this.state.notes.filter(note => note.id !== id)
+    });
+  };
 
   render() {
     return (
-      <AppContext.Provider value={this.state}>
+      <AppContext.Provider
+        value={Object.assign({}, this.state, {
+          handleDelete: this.handleDelete
+        })}
+      >
         <div className="App">
           <header>
-            <Link to="/"><h1>Noteful</h1></Link>
+            <Link to="/">
+              <h1>Noteful</h1>
+            </Link>
           </header>
           <Nav />
           <section className="app-content">
             <main>
-              <Route
-                exact path="/"
-                render={routeProps => <Main handleDelete={this.handleDelete} {...routeProps} />} />
-              <Route
-                path="/folder/:folderId"
-                component={Folder} />
-              <Route
-                path="/note/:noteId"
-                component={NotePage} />
+              <Route exact path="/" component={Main} />
+              <Route path="/folder/:folderId" component={Folder} />
+              <Route path="/note/:noteId" component={NotePage} />
             </main>
           </section>
         </div>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,42 +4,49 @@ import './Main.css';
 import AppContext from '../AppContext';
 
 export default class Main extends Component {
+  static contextType = AppContext;
+  handleDeleteButton = id => {
+    const url = `http://localhost:9090/notes/${id}`;
+    fetch(url, {
+      method: 'DELETE'
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(response.statusText);
+        }
+      })
+      .then(() => this.context.handleDelete(id));
+  };
 
-    handleDeleteButton = (e, id) => {
-        const url = `http://localhost:9090/notes/${id}`
-        fetch(url, {
-            method: 'DELETE'
-        })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error(response.statusText);
-            }
-        })
-        .then(responseJson => this.props.handleDelete(responseJson));
-    }
-
-    render() {
-        return (
-            <AppContext.Consumer>
-                {({ notes }) => {
-                    return (notes.map((note, index) => {
-                        return (
-                            <main className="notes-display" key={index}>
-                                <section className="note">
-                                    <Link to={`/note/${note.id}`}>
-                                        <h3>{note.name}</h3>
-                                    </Link>
-                                    <p>Date Modified: {note.modified}</p>
-                                    <button onClick={(e, id) => this.handleDeleteButton(e, note.id)} className="delete-button">Delete Note</button>
-                                </section>
-                                <button>Add Note</button>
-                            </main>
-                        )
-                    })
-                )} }
-            </AppContext.Consumer>
-        )
-    }
+  render() {
+    console.log(this.props);
+    return (
+      <AppContext.Consumer>
+        {({ notes }) => {
+          console.log(this.context);
+          return notes.map((note, index) => {
+            return (
+              <main className="notes-display" key={index}>
+                <section className="note">
+                  <Link to={`/note/${note.id}`}>
+                    <h3>{note.name}</h3>
+                  </Link>
+                  <p>Date Modified: {note.modified}</p>
+                  <button
+                    onClick={id => this.handleDeleteButton(note.id)}
+                    className="delete-button"
+                  >
+                    Delete Note
+                  </button>
+                </section>
+                <button>Add Note</button>
+              </main>
+            );
+          });
+        }}
+      </AppContext.Consumer>
+    );
+  }
 }
